940e352fef78efa2b8158f7e204c09117715fe46
mingliang.wang <mingliang_xyz@outlook.com>
2022-03-23 09:48:48 +0800

    修复bug

--- KanBan/KanBan/js/echarts/workorderinfocharts.js
+++ KanBan/KanBan/js/echarts/workorderinfocharts.js
@@ -1,4 +1,4 @@
-﻿const { color } = require("./echarts");
+﻿const { color } = require("./echarts.js");
 
 function char0x() {
     return {
@@ -184,7 +184,6 @@ function char0x() {
     }
 }
 
-//温度折线图
 function char0y() {
     return {
         title: {
@@ -894,7 +893,7 @@ function char02() {
                     show: true,
                     position: 'top',
                     textStyle: {
-                        //color: '#fff',
+                        color: '#fff',
                     }
                 }
             },
@@ -1133,13 +1132,19 @@ function char07(value) {
     }
     var option = {
         title: {
-            text: '湿度',
+            text: '湿度',//
             left: 'right',
             textStyle: {
                 fontSize: 24,
-                color:"green"
+                color: "green"
             }
 
+        },
+        grid: {
+            //left: '15%', // 与容器左侧的距离
+            right: '15%', // 与容器右侧的距离
+            //top: '5%', // 与容器顶部的距离
+            //bottom: '5%', // 与容器底部的距离
         },
         series: [{
             type: 'liquidFill',
@@ -1149,14 +1154,17 @@ function char07(value) {
             //    color: "white"
             //},
             label: {
+
                 normal: {
                     formatter: (value).toFixed(1) + "%",
-                    color: wave_color
+                  /*  color: wave_color*/
                 },
                 textStyle: {
-                    fontSize: 14,
-                    color: wave_color
-                }
+                    //fontSize: 14,
+                    //color: wave_color
+                },
+                fontSize: 25,
+                color: "#99ff33"
             },
             outline: {
                 show: false
@@ -1170,6 +1178,306 @@ function char07(value) {
 
 //温度计
 function char08(value) {
+    var TP_value = value;
+    var kd = [];
+    var Gradient = [];
+    var leftColor = '';
+    var showValue = '';
+    var boxPosition = [75, -50];//温度文字位置
+    var TP_txt = ''
+    // 刻度使用柱状图模拟，短设置1，长的设置3；构造一个数据
+    for (var i = 0, len = 110; i <= len; i++) {
+        if (i < 10 || i > 130) {
+            kd.push('')
+        } else {
+            if ((i) % 20 === 0) {
+                kd.push('-2');
+            } else if ((i - 10) % 4 === 0) {
+                kd.push('-5');
+            }
+            else {
+                kd.push('');
+            }
+        }
+
+    }
+    //中间线的渐变色和文本内容
+    if (TP_value > 28) {
+
+        TP_txt = '温度偏高';
+        Gradient.push({
+            offset: 0,
+            color: '#93FE94'//浅绿
+        }, {
+            offset: 0.5,
+            color: '#E4D225' //浅黄
+        }, {
+            offset: 1,
+            color: '#E01F28'//红色
+        })
+    } else if (TP_value > 20) {
+
+        TP_txt = '温度正常';
+        Gradient.push({
+            offset: 1,
+            color: '#93FE94'
+        })
+  
+    } else {
+
+        TP_txt = '温度偏低';
+        Gradient.push({
+            offset: 0,
+            color: '#93FE94'
+        }, {
+            offset: 0.5,
+            color: '#E4D225'
+        }, {
+            offset: 1,
+            color: '#E01F28'
+        })
+    }
+
+    ////模拟水银柱
+    //if (TP_value > 62) {
+    //    showValue = 62
+    //} else {
+    //    if (TP_value < -60) {
+    //        showValue = -60
+    //    } else {
+    //        showValue = TP_value
+    //    }
+    //}
+    //if (TP_value < -10) {
+    //    boxPosition = [65, -120];
+    //}
+    leftColor = Gradient[Gradient.length - 1].color;
+    // 因为柱状初始化为0，温度存在负值，所以加上负值60和空出距离10
+    var option = {
+        backgroundColor: '',
+        title: {
+            text: '温度',//
+            show: true,
+            //align: "center",
+            left: 'left',
+            textStyle: {
+                fontSize: 24,
+                color: "green"
+            }
+        },
+        grid: {
+            left: '16%', // 与容器左侧的距离
+            //right: '5%', // 与容器右侧的距离
+            //top: '5%', // 与容器顶部的距离
+            //bottom: '5%', // 与容器底部的距离
+        },
+        yAxis: [{
+            show: false,
+            data: [],
+            min: 0,
+            max: 135,
+            axisLine: {
+                show: false
+            }
+        }, {
+            show: false,
+            min: 0,
+            max: 50,
+        }, {
+            type: 'category',
+            data: ['', '', '', '', '', '', '', '', '', '', '°C'],
+            position: 'left',
+            offset: -80,
+            axisLabel: {
+                fontSize: 10,
+                color: 'white'
+            },
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+        }],
+        xAxis: [{
+            show: false,
+            min: -10,
+            max: 80,
+            data: []
+        }, {
+            show: false,
+            min: -10,
+            max: 80,
+            data: []
+        }, {
+            show: false,
+            min: -10,
+            max: 80,
+            data: []
+        }, {
+            show: false,
+            min: -5,
+            max: 80,
+
+        }],
+        series: [{
+            name: '条',
+            type: 'bar',
+            // 对应上面XAxis的第一个对)象配置
+            xAxisIndex: 0,
+            data: [{
+                value: (showValue + 30),
+                label: {
+                    normal: {
+                        show: true,
+                        position: boxPosition,
+                        width: 20,
+                        height: 100,
+                        formatter: '{back| ' + TP_value + ' }{unit|°C}\n{downTxt|' + '}',
+                        rich: {
+                            back: {
+                                align: 'center',
+                                lineHeight: 50,
+                                fontSize: 30,
+                                fontFamily: 'digifacewide',
+                                color: leftColor
+                            },
+                            unit: {
+                                fontFamily: '微软雅黑',
+                                fontSize: 15,
+                                lineHeight: 50,
+                                color: leftColor
+                            },
+                            downTxt: {
+                                lineHeight: 50,
+                                fontSize: 25,
+                                align: 'center',
+                                color: '#fff'
+                            }
+                        }
+                    }
+                }
+            }],
+
+            barWidth: 18,
+            itemStyle: {
+                normal: {
+                    color: new echarts.graphic.LinearGradient(0, 1, 0, 0, Gradient)
+                }
+            },
+            z: 2
+        }, {
+            name: '白框',
+            type: 'bar',
+            xAxisIndex: 1,
+            barGap: '-100%',
+            data: [134],
+            barWidth: 28,
+            itemStyle: {
+                normal: {
+                    color: '#0C2E6D',
+                    barBorderRadius: 50,
+                }
+            },
+            z: 1
+        }, {
+            name: '外框',
+            type: 'bar',
+            xAxisIndex: 2,
+            barGap: '-100%',
+            data: [135],
+            barWidth: 38,
+            itemStyle: {
+                normal: {
+                    color: '#4577BA',
+                    barBorderRadius: 50,
+                }
+            },
+            z: 0
+        }, {
+            name: '圆',
+            type: 'scatter',
+            hoverAnimation: false,
+            data: [0],
+            xAxisIndex: 0,
+            symbolSize: 48,
+            itemStyle: {
+                normal: {
+                    color: '#93FE94',
+                    opacity: 1,
+                }
+            },
+            z: 2
+        }, {
+            name: '白圆',
+            type: 'scatter',
+            hoverAnimation: false,
+            data: [0],
+            xAxisIndex: 1,
+            symbolSize: 60,
+            itemStyle: {
+                normal: {
+                    color: '#0C2E6D',
+                    opacity: 1,
+                }
+            },
+            z: 1
+        }, {
+            name: '外圆',
+            type: 'scatter',
+            hoverAnimation: false,
+            data: [0],
+            xAxisIndex: 2,
+            symbolSize: 70,
+            itemStyle: {
+                normal: {
+                    color: '#4577BA',
+                    opacity: 1,
+                }
+            },
+            z: 0
+        }, {
+            name: '刻度',
+            type: 'bar',
+            yAxisIndex: 0,
+            xAxisIndex: 3,
+            label: {
+                normal: {
+                    show: true,
+                    position: 'left',
+                    distance: 15,
+                    color: 'white',
+                    fontSize: 14,
+                    formatter: function (params) {
+                        if (params.dataIndex > 130 || params.dataIndex < 10) {
+                            return '';
+                        } else {
+                            if ((params.dataIndex - 10) % 30 === 0) {
+                                return params.dataIndex - 10;
+                            } else {
+                                return '';
+                            }
+                        }
+                    }
+                }
+            },
+            barGap: '-100%',
+            data: kd,
+            barWidth: 1,
+            itemStyle: {
+                normal: {
+                    color: 'white',
+                    barBorderRadius: 120,
+                }
+            },
+            z: 0
+        }]
+    };
+    return option;
+}
+
+//温度计
+function char0801(value) {
     var TP_value = value;
     var kd = [];
     var Gradient = [];
@@ -1215,7 +1523,7 @@ function char08(value) {
             color: '#93FE94'
         })
 
-      
+
     } else {
 
         TP_txt = '温度偏低';
@@ -1249,7 +1557,7 @@ function char08(value) {
     var option = {
         backgroundColor: '',
         title: {
-            text: '温度',
+            text: '温度',//
             show: true,
             //align: "center",
             left: 'left',
